b9f7e5d2ea1e3320a2ce3cc2deac07c8bf88fa1b
asemin08 <semin.allan@gmail.com>
2021-11-27 16:21:35 +0100

    Ajout fichier pour l'ec2 + variables ec2 comme tp 3 et 4

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,42 @@
+variable "auteur" {
+  type    = string
+  default = "allan"
+}
+
+variable "zone-dispo" {
+  type    = string
+  default = "us-east-1b"
+}
+
+variable "type_instance" {
+  type    = string
+  default = "t2.nano"
+}
+
+variable "cle_ssh" {
+  type    = string
+  default = "allan-kp"
+}
+
+variable "securite_groupe" {
+  type    = string
+  default = "NULL"
+}
+
+variable "ip_public" {
+  type    = string
+  default = "NULL"
+}
+
+variable "utilisateur_ssh" {
+  type    = string
+  default = "NULL"
+}
+
+variable "id_compte_ubuntu" {
+  default = "099720109477"
+}
+
+variable "nom_ubuntu_ami" {
+  default = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+}
